cad9e85f0d40fd1770320c5845f340433761ae20
fabio <fabiuslongo@gmail.com>
2025-04-10 17:03:26 +0200

    +DTWIN window

--- DTWIN/dtwin_win.py
+++ DTWIN/dtwin_win.py
@@ -17,7 +17,7 @@ from the text of a scene beliefs related to verb can have two arguments. For exa
 """
 
 # Goal system
-# Formulate a single goal belief from the scene (without other text) in the shape of predicate, with single-words label related to an agent from the text of the scene. Verbs can have two arguments. For example: "The car runs on the highway —→ AGENT(CAR),  RUN(CAR, HIGHWAY)"
+# Formulate an optimal-single goal from the scene (without other text) in the shape of predicate, with single-words label related to an agent from the text of the scene. Verbs can have two arguments. For example: "The car runs on the highway —→ AGENT(CAR),  RUN(CAR, HIGHWAY)"
 
 multimodal_model = "llava:13b-v1.6-vicuna-q8_0"
 text_model = "qwen2.5:14b-instruct-q8_0"
